17f16008576552386467430ce6a10ca5b1ff67e3
jamalamar <jamal_ab22@hotmail.com>
2019-02-11 13:12:43 -0600

    working form

--- react-forms-lecture/src/Login/index.js
+++ react-forms-lecture/src/Login/index.js
@@ -13,22 +13,21 @@ export default class Login extends Component {
     handleSubmit = (event) => {
     // stop page from refreshing
     event.preventDefault();
+    this.props.login(this.state.username);
 
-    this.props.login();
+    }
 
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      [event.currentTarget.name]: event.currentTarget.value
-    })
-  }
+    handleChange = (event) => {
+      this.setState({
+        [event.currentTarget.name]: event.currentTarget.value
+      })
+    }
 
   render() {
     return (
     <form onSubmit={this.handleSubmit}>
-        <input type='text' name='username' placeholder='Username' value={this.state.userName} onChange={this.handleChange}/>
-        <input type='password' name='password' placeholder='Password' value={this.state.password} onChange={this.handleChange}/>
+        <input type='text' name='username' placeholder='username' value={this.state.username} onChange={this.handleChange}/>
+        <input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.handleChange}/>
         <input type='submit' value='Submit'/>
     </form>
     );
